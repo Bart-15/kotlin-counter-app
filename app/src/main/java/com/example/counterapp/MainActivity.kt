4fed1abafc2a7a53333c8cf2ad6939cd3cd1d997
Bart-15 <brat.tabusao@gmail.com>
2026-01-04 19:57:59 +0800

    chore: 💬 remove spacing

--- app/src/main/java/com/example/counterapp/MainActivity.kt
+++ app/src/main/java/com/example/counterapp/MainActivity.kt
@@ -17,7 +17,6 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CounterAppTheme {
-
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     CounterScreen(
                         modifier = Modifier.padding(innerPadding)
